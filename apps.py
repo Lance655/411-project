187a55d4defc8b3a9c699eb38c797eea5ccda6dc
CecileYitingLuo <146459429+CecileYitingLuo@users.noreply.github.com>
2023-11-28 11:12:03 -0500

    Update apps.py

--- apps.py
+++ apps.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request, jsonify, Response
 import requests
-from backend import get_weather_for_date, calculate_weather_score
+from backend import get_weather_for_date, calculate_weather_score, interpret_weather_score
 import datetime
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='', template_folder='')
 
 
 @app.route('/')
@@ -15,11 +15,20 @@ def index():
 def find_events():
     target_date = request.form.get('event-date')
     city = request.form.get('city')
-    weather_info = get_weather_for_date("TODO",  city, target_date)
+    weather_info = get_weather_for_date("TODO api_key",  city, target_date)
     weather_score = calculate_weather_score(weather_info)
     return render_template('events.html',weather_score=weather_score)
 
 
+@app.route('/get_weather_categorizes', methods=["GET", "POST"])
+def get_weather_categorizes():
+    city = request.args.get('city')
+    target_date = request.args.get('date')
+    #eventName = request.args.get('eventName')
+
+    weather_score = calculate_weather_score("TODO api_key", city, target_date)
+    weather_categorizes = interpret_weather_score(weather_score)
+    return weather_categorizes
 
 if __name__ == "__main__":
     app.run(port=8080, debug=True)
